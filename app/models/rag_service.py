4d9548cb965dafb61ad2f2c67560680bba84815b
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-14 17:17:54 +0530

    Update rag_service.py

--- app/models/rag_service.py
+++ app/models/rag_service.py
@@ -6,15 +6,18 @@ import uuid
 RAG = RAGMultiModalModel.from_pretrained("vidore/colqwen2-v1.0", verbose=1)
 
 def index_pdf(path: str):
-    unique_name = f"uploaded_{uuid.uuid4().hex[:8]}"
+    index_name = f"uploaded_{uuid.uuid4().hex[:8]}"
     RAG.index(
         input_path=path,
-        index_name=unique_name,
+        index_name=index_name,
         store_collection_with_index=True,
         overwrite=True
     )
+    return index_name
 
-def search_image(question: str):
+def search_image(question: str, index_name: str):
     results = RAG.search(question, k=1, index_name=index_name)
+    if not results or not results[0].base64:
+        raise ValueError(f"No image found for query: {question}")
     image_bytes = base64.b64decode(results[0].base64)
     return Image.open(io.BytesIO(image_bytes))
